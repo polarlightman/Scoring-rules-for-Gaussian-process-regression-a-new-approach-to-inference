174db23844be14f69ab5428d9304bb13d4c3dcf3
JerryHuang <huang.commanding@qq.com>
2018-09-04 21:31:12 +0100

    Add files via upload

---
+++ crps-plot.R
@@ -0,0 +1,66 @@
+
+
+
+
+
+mean=1152
+sd=84
+x <- seq(-3.5,3.5,length=10000)*sd + mean
+y <- dnorm(x,mean,sd)
+
+
+plot(x, y, type="l")
+polygon(c( x[x>=1250], max(x) ),  c(y[x==max(x)], y[x>=1250] ), col="red")
+
+polygon(c( x[x>=1250], 1250 ), 
+        c(y[x>=1250],0 ), col="red")
+
+
+
+###############################################
+
+par(mfrow=c(1,2))
+
+
+z <- pnorm(x,mean,sd)
+k <- rnorm(10000)
+
+plot(ecdf(k),lwd=3,col='red',xlab='',ylab='CDF',main='Probabilistic Forecast')
+x <- seq(-5,5,length=100000)
+y <- pnorm(x)
+x0 <- 0.5
+lines(c(-5,x0),c(0,0),type='l',lwd=3)
+lines(c(x0,x0),c(0,1),type='l',lwd=3)
+lines(c(x0,5),c(1,1),type='l',lwd=3)
+
+polygon(c( x[x<=x0], x0 ), 
+        c(y[x<=x0],0 ), col="gray87")
+polygon(c( x[x>=x0], x0 ), 
+        c(y[x>=x0],1 ), col="gray87")
+lines(ecdf(k),lwd=3,col='red')
+
+
+
+plot(c(-5,5),c(0,1),xlab='',ylab='CDF',main='Deterministic Forecast',type='n')
+abline(h=1,lty=2);abline(h=0,lty=2)
+yjitter1 <-0.997
+yjitter2 <--0.003 
+lines(c(-5,x0),c(yjitter2,yjitter2),type='l',lwd=4)
+lines(c(x0,x0),c(0,1),type='l',lwd=4)
+lines(c(x0,5),c(yjitter1,yjitter1),type='l',lwd=4)
+
+xp <- -1.4
+lines(c(-5,xp),c(0,0),type='l',lwd=4,col='red')
+lines(c(xp,xp),c(0,1),type='l',lwd=6,col='red')
+lines(c(xp,5),c(1,1),type='l',lwd=4,col='red')
+
+polygon(c( xp,x0, x0,xp ), 
+        c(0,0,0.999,0.999), col="gray87")
+
+
+###############################################
+
+
+
+
+
